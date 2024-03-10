27323022cf6f8c131e5754cceb74545c482553e6
Mukilan1win23 <mukilan.2.2014@gmail.com>
2024-03-10 18:36:38 -0500

    Install dark Sci-Server

--- install-dark.sh
+++ install-dark.sh
@@ -1,5 +1,5 @@
 echo '┌───── •✧✧• ─────┐
- -SCI-SERVER- 
+ -SCI-SERVER INSTALLER- 
 └───── •✧✧• ─────┘'
 
 readonly MINIMUM_DISK_SIZE_GB="5"
@@ -11,11 +11,28 @@ echo '┌───── •✧✧• ─────┐
  -UPDATING- 
 └───── •✧✧• ─────┘'
 
-apt update
-apt upgrade
-apt upgrade dist
-apt full-upgrade
+apt update -y
+apt upgrade -y
+apt upgrade dist -y
+apt full-upgrade -y
 
 echo '┌───── •✧✧• ─────┐
- -DONE UPDATING- 
+ -INSTALLING DEPENDENCIES- 
 └───── •✧✧• ─────┘'
+
+apt install libfuse2 -y
+
+echo '┌───── •✧✧• ─────┐
+ -INSTALLING PACKAGES- 
+└───── •✧✧• ─────┘'
+
+apt install apache2 openjdk-17-jdk tomcat10 tomcat10-docs webalizer php8.2 php-mysql -y
+
+echo '┌───── •✧✧• ─────┐
+ -INSTALLING NVM- 
+└───── •✧✧• ─────┘'
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
